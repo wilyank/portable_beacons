caebacbace1d5893a6388769c6a358f6d484ddf3
wilyank <72388958+wilyank@users.noreply.github.com>
2021-12-07 09:34:24 +0100

    texture update

--- src/main/java/wilyan_kramer/portable_beacons/setup/CommonSetup.java
+++ src/main/java/wilyan_kramer/portable_beacons/setup/CommonSetup.java
@@ -8,16 +8,17 @@ import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import wilyan_kramer.portable_beacons.PortableBeaconsMod;
 import wilyan_kramer.portable_beacons.common.command.ModCommands;
 
+
 @Mod.EventBusSubscriber(modid = PortableBeaconsMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CommonSetup {
 	
 	public static void init(final FMLCommonSetupEvent event) {
-		
 	}
 	
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
     }
+    
     
     @SubscribeEvent
     public void registerCommands(RegisterCommandsEvent event) {
